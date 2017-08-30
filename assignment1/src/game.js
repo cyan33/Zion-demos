0043f8d3adb3a6e60ddfccd3ac0f30c6a09034ad
thomasyimgit <thomasyim94@gmail.com>
2017-08-30 19:53:51 -0400

    add options.js and basic layout css

--- assignment1/src/game.js
+++ assignment1/src/game.js
@@ -1,10 +1,18 @@
+import {
+    EMOJI_AMOUT
+} from './options'
+
 class Game {
     constructor() {
         this.canvas = document.getElementById('alchemy_table');
         this.context = canvas.getContext('2d');
     }
 
+    insertEmojisToSidebar() {
+    }
+    
     init() {
+
         
     }
 }
