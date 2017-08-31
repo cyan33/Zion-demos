62f2d72e1118672e128b89f92838ada15f37c889
Josh Good <jegood@ncsu.edu>
2017-08-31 00:08:07 -0400

    Removing unnecessary default values on Sprite class.

--- assignment1/src/utils/sprite.js
+++ assignment1/src/utils/sprite.js
@@ -1,5 +1,5 @@
 class Sprite {
-    constructor(x = 0, y = 0, width = 64, height = 64, src = undefined) {
+    constructor(x, y, width, height, src) {
         this.X = x;
         this.Y = y;
         this.image = new Image();
