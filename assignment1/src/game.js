3b2f9abcff6de5775395aa716a735aa50465586c
thomasyimgit <thomasyim94@gmail.com>
2017-08-30 21:42:12 -0400

    sidebar emoji and inject

--- assignment1/src/game.js
+++ assignment1/src/game.js
@@ -1,28 +1,52 @@
 import {
     EMOJI_AMOUNT,
-    EMOJI_URL_SOURCE
+    EMOJI_URL_SOURCE,
+    EMOJI_NAME
 } from './options'
+
+import {
+    generateRandomPosition
+} from './utils/canvas'
 
 class Game {
     constructor() {
-        // this.canvas = document.getElementById('alchemy_table');
-        // this.context = canvas.getContext('2d');
+        this.canvas = document.getElementById('alchemy_table');
+        this.context = this.canvas.getContext('2d');
+    }
+
+    addSidebarEmojiClickHandler(e) {
+        // get the index of the clicked emoji
+        let target = e.target;
+
+        const { x, y } = generateRandomPosition(this.canvas, true);
+        
+        this.context.drawImage(target, x, y, 64, 64);
     }
 
     insertEmojisToSidebar() {
         for (let i = 0; i < EMOJI_AMOUNT; i++) {
             let div = document.createElement('div');
-            div.className = 'sidebar-emoji';
+            div.className = `sidebar-emoji ${EMOJI_NAME[i]}`;
             let img = document.createElement('img');
             img.src = EMOJI_URL_SOURCE[i];
             div.append(img);
+
+            div.addEventListener('click', (e) => this.addSidebarEmojiClickHandler(e));
 
             document.querySelector('.sidebar').append(div);
         }
     }
     
+    debug() {
+        window.canvas = this.canvas;
+        window.context = this.context;
+    }
+
     init() {
         this.insertEmojisToSidebar();
+
+        // only activate this function when you are in developing mode
+        this.debug();
     }
 }
 
--- assignment1/src/utils/canvas.js
+++ assignment1/src/utils/canvas.js
@@ -10,4 +10,16 @@ export function coordinateConversion(canvas, x, y) {
     }
 }
 
+export function generateRandomPosition(canvas, middle = false) {
+    let x, y;
+    const getRandomNumBetween = (min, max) => Math.random() * (max - min) + min; 
 
+    if (middle) {
+        x = Math.round(getRandomNumBetween(canvas.width * 0.2, canvas.width * 0.8));
+        y = Math.round(getRandomNumBetween(canvas.height * 0.2, canvas.height * 0.8));
+    } else {
+        x = Math.round(getRandomNumBetween(0, canvas.width));
+        y = Math.round(getRandomNumBetween(0, canvas.height));
+    }
+    return { x, y };
+}
