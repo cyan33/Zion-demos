45aefe799c6cc47fcefdae1c0ad2e8eada1b464d
cjvalent <coreyjasvalentine@gmail.com>
2017-11-30 12:44:54 -0500

    Basic rendering and IO implementations

--- assignment4_CopsAndRobbers/src/CRGame.js
+++ assignment4_CopsAndRobbers/src/CRGame.js
@@ -1,10 +1,74 @@
+import {
+    CANVAS_HEIGHT,
+    CANVAS_WEIGHT,
+    UP, DOWN, LEFT, RIGHT
+} from './options'
+
+import { createImageCache } from './engine/canvas'
+import Game from './engine/Game'
+
 class CRGame {
     constructor() {
+        super();
+        this.canvas = document.querySelector('#copsAndRobbers');
+        this.context = this.canvas.getContext('2d');
+        this.gameover = false;
+        this.gameloop = this.gameloop.bind(this);
+
+        // Start player off looking right? Does it matter?
+        this.direction = RIGHT;
+    }
+
+    initImageCache() {
+        this.imageCache = createImageCache();
+
+        const IMAGE_DICT = {
+            // cops and robbers images here
+        }
+
+        Object.keys(IMAGE_DICT).forEach(name => {
+            this.imageCache.loadImage(name, IMAGE_DICT[name]);
+        })
+
+        this.imageCache.imagesOnLoad(() => this.timer = setInterval(this.gameloop, 30));
+    }
+
+    addKeyboardHandlers() {
+        document.addEventListener('keydown', (e) => this.setMovingDirection(e));
+    }
+
+    setMovingDirection(e) {
+        if (e.keyCode === 37) {
+            this.direction = LEFT;
+        } else if (e.keyCode === 38) {
+            this.direction = UP;
+        } else if (e.keyCode === 39) {
+            this.direction = RIGHT;
+        } else if (e.keyCode === 40) {
+            this.direction = DOWN;
+        }
+    }
+
+    update() {
         // TODO
     }
 
+    // rendering the game
+    render() {
+        const { width, height } = this.canvas;
+
+        let images = {};
+
+        this.imageCache.getImages().forEach(item => {
+            images[item.name] = item.img;
+        })
+
+        // draw cops and robbers here
+    }
+
     init() {
-        // TODO
+        this.initImageCache();
+        this.addKeyboardHandlers();
     }
 
 }
---
+++ assignment4_CopsAndRobbers/src/options.js
@@ -0,0 +1,9 @@
+// canvas parameters
+export const CANVAS_HEIGHT = 600;
+export const CANVAS_WIDTH = 600;
+
+// keycodes
+export const LEFT = 37;
+export const UP = 38;
+export const RIGHT = 39;
+export const DOWN = 40;
