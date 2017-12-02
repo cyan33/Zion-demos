a4c94ebc37bbfdd2587b616c87b361026bec18ab
Josh Good <jegood@ncsu.edu>
2017-12-01 21:08:13 -0500

    All characters now store their canvas location and grid index

--- assignment4_CopsAndRobbers/src/helper.js
+++ assignment4_CopsAndRobbers/src/helper.js
@@ -1,30 +1,6 @@
 import {getRandomNumber} from './engine/operations'
-import {MAX_PLAYERS, CANVAS_WIDTH, CANVAS_HEIGHT, GRID, OPEN, WALL, COP, ROBBER,
+import {CANVAS_WIDTH, CANVAS_HEIGHT, GRID, OPEN, WALL, COP, ROBBER,
         EXIT, UP, DOWN, LEFT, RIGHT} from './options'
-
-/**
- * Purely for testing purposes. This will be updated
- * with the real spawn locations once the map is created.
- */
-export function initSpawnLocations() {
-    let locations = [];
-    while(locations.length != MAX_PLAYERS) {
-        // get a random x, y location
-        let spawnX = getRandomNumber(CANVAS_WIDTH);
-        let spawnY = getRandomNumber(CANVAS_HEIGHT);
-        let valid = true;
-        for(let i = 0; i < locations.length; i++) {
-            let spawn = locations[i];
-            if(spawn.x === spawnX && spawn.y === spawnY) valid = false
-        }
-        // Only add the new spawn if we are valid
-        if(valid) {
-            locations.push({x: spawnX, y: spawnY, occupied: false});
-        }
-    }
-
-    return locations;
-}
 
 export function getSpawnLocation(spawnLocations) {
     let index = 0;
@@ -34,6 +10,12 @@ export function getSpawnLocation(spawnLocations) {
         if(!spawnLocations[index].occupied) found = true;
     }
     return index;
+}
+
+export function convertGridToPixelCoords(cell) {
+    let xCorner = CANVAS_WIDTH / GRID[0].length * cell.c;
+    let yCorner = CANVAS_HEIGHT / GRID.length * cell.r;
+    return {x: xCorner, y: yCorner};
 }
 
 export function drawWalls(context, width, height) {
